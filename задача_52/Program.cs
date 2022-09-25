37286c22328e080eae4ea7ad086950ab58b79996
Alena <qlushenko36@gmail.com>
2022-09-26 02:02:49 +0300

    задача 52

---
+++ задача_52/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] matrix = new int[3,4];
+
+FillArray2(matrix);
+PrintArray2(matrix);
+
+double[] avgNumbers = new double[matrix.GetLength(1)];
+
+for (int i = 0; i < matrix.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        result += matrix[j, i];
+    }
+    avgNumbers[i] = result / matrix.GetLength(0);
+}
+PrintArray(avgNumbers);
+
+void FillArray2(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        string result = string.Format("{0:f1}", array[i]);
+        Console.Write(result + "; ");
+    }
+    Console.WriteLine();
+}
